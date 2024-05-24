7d9cc1e4facf4111e6c4450a3ed9a61764a5be09
Tomiwa Adelae <thetommedia@gmail.com>
2024-05-24 15:42:33 +0100

    Preparing for deployment

--- app/faqs/_components/Faq.tsx
+++ app/faqs/_components/Faq.tsx
@@ -10,15 +10,15 @@ import {
 const Faq = ({ question, answer }: { question: string; answer: string }) => {
 	return (
 		<Accordion
-			className="bg-slate-50 transition ease-in-out px-4 rounded-xl  hover:bg-slate-100 mb-4"
+			className="bg-slate-50 transition ease-in-out px-4 rounded-xl  hover:bg-slate-100 mb-4 text-left"
 			type="single"
 			collapsible
 		>
 			<AccordionItem value="item-1">
-				<AccordionTrigger className="text-sm md:text-base hover:no-underline text-green-400">
+				<AccordionTrigger className="text-sm text-left md:text-base hover:no-underline text-green-400">
 					{question}
 				</AccordionTrigger>
-				<AccordionContent className="text-xs md:text-sm">
+				<AccordionContent className="text-xs text-left md:text-sm">
 					{answer}
 				</AccordionContent>
 			</AccordionItem>
